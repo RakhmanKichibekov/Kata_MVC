f48d3d12572d40edc67e3683a7e8f4e64e254858
RakhmanKichibekov <kichibekovrahman@yandex.ru>
2024-02-27 18:17:26 +0300

    fix HelloController

--- src/main/java/web/controller/HelloController.java
+++ src/main/java/web/controller/HelloController.java
@@ -7,17 +7,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import java.util.ArrayList;
 import java.util.List;
 
-//Находясь в среде разработки, табуляция у этого класса нормальная, но на гите она почему-то сдвигается
 @Controller
 public class HelloController {
 	
-	@GetMapping(value = "/")
-		public String printWelcome(ModelMap model) {
-		List<String> messages = new ArrayList<>();
-		messages.add("Hello!");
-		messages.add("I'm Spring MVC application");
-		messages.add("5.2.0 version by sep'19 ");
-		model.addAttribute("messages", messages);
-		return "index";
-	}
+    @GetMapping(value = "/")
+        public String printWelcome(ModelMap model) {
+        List<String> messages = new ArrayList<>();
+        messages.add("Hello!");
+        messages.add("I'm Spring MVC application");
+        messages.add("5.2.0 version by sep'19 ");
+        model.addAttribute("messages", messages);
+        return "index";
+    }
 }
